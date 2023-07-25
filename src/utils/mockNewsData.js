b580e737573f2e2a65b77d6034e7f1b568e5e607
Roxi <roxana_ghenea24@yahoo.com>
2023-07-25 20:41:29 +0300

    json-server setup

---
+++ src/utils/mockNewsData.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const getArticlesData = async () => {
+    try {
+        const responseData = await axios.get(`http://localhost:3000/mockApiResponse`);
+        return responseData.data.response.results;
+
+    }
+    catch (error) {
+        return error;
+    }
+}
